de2fb3294179e7a7b310dae4e096e23f9ac3b848
Liz <lizlarkin@highlandtechnology.com>
2020-10-16 16:14:44 -0700

    added conditional push of character options into temp password

--- script.js
+++ script.js
@@ -14,37 +14,50 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Collect User Preference
-let lengthUserInput = prompt("How long do you want the password between 8 and 128 characters?")
+var lengthUserInput = prompt("How long do you want the password between 8 and 128 characters?")
   console.log(lengthUserInput);
  
-let lowercaseUserInput = confirm("Do you want to include lowercase letters?")
+var lowercaseUserInput = confirm("Do you want to include lowercase letters?")
   console.log(lowercaseUserInput);
 
-let uppercaseUserInput = confirm("Do you want to include uppercase letters?")
+var uppercaseUserInput = confirm("Do you want to include uppercase letters?")
   console.log(uppercaseUserInput);
 
-let numericUserInput = confirm("Do you want to include numeric characters?")
+var numericUserInput = confirm("Do you want to include numeric characters?")
   console.log(numericUserInput);
 
-let specialCharUserInput = confirm("Do you want to include special characters?")
+var specialCharUserInput = confirm("Do you want to include special characters?")
   console.log(specialCharUserInput);
 
 // Set password options
 
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   console.log(letters);
 
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  console.log(upperLetters);
+
+// for (let i = 0; i < letters.length; i++) {
+//     console.log(letters[i].toUpperCase());
+  
+// }
+
+// var upperLetters = function(letters) {
+//   return letters.toUpperCase();
+// }
+
+//   console.log(upperLetters);
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   console.log(numbers);
 
-let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
   console.log(specialCharacters);
 
-// Calculate number of each desired parameter to append to password
-// let countTrue = function() {
-let countTrue = 0;
+// Calculate number of each requested parameter to append to password
+var countTrue = 0;
     if (lowercaseUserInput === true) {
-      countTrue++;
+      countTrue++
   } if (uppercaseUserInput === true) {
     countTrue++;
   } if (numericUserInput === true) {
@@ -52,15 +65,31 @@ let countTrue = 0;
   } if (specialCharUserInput === true) {
     countTrue++;
   }
-  console.log(countTrue)
-// }
+  console.log(lengthUserInput / countTrue)
+
+// Push user requested characters to tempPass variable
+// If confirm is true, push characters from array at random based on length
+
+var tempPass = [] 
+  if (lowercaseUserInput === true) {
+    tempPass.push(letters);
+  } 
+  if (uppercaseUserInput === true) {
+    tempPass.push(upperLetters);
+  } 
+  if (numericUserInput === true) {
+    tempPass.push(numbers);
+  } 
+  if (specialCharUserInput === true) {
+    tempPass.push(specialCharacters);
+  } 
+  
+  
+  console.log(tempPass)
+// Edit tempPass variablet o generate final password based on user requests
 
 
-// let lengthParameter function()
 
 
-
-// Set variable to hold password  
-let testpass = [] 
-  console.log(testpass)
+ 
 
