b450620571340341312fe71b7e1827ada6ee4ffa
Liz <lizlarkin@highlandtechnology.com>
2020-10-17 08:01:16 -0700

    added for loops to randomize characters from character option arrays

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Collect User Preference
+// Collect User Preferences
 var userInput = parseInt(prompt("How long do you want the password between 8 and 128 characters?"))
     if (userInput >= 8 && userInput <= 128) {
       var lengthUserInput = userInput;  
@@ -22,32 +22,17 @@ var userInput = parseInt(prompt("How long do you want the password between 8 and
         console.log(lowercaseUserInput);
       var uppercaseUserInput = confirm("Do you want to include uppercase letters?")
         console.log(uppercaseUserInput);
-        var numberUserInput = confirm("Do you want to include numbers?")
-        console.log(numberUserInput);     
+        var numericUserInput = confirm("Do you want to include numbers?")
+        console.log(numericUserInput);     
       var specialCharUserInput = confirm("Do you want to include special characters?")
         console.log(specialCharUserInput);     
     } else alert("Please input numbers between 8 and 128")
 
-
 // Define password character options
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-// for (let i = 0; i < letters.length; i++) {
-//     console.log(letters[i].toUpperCase());
-  
-// }
-
-// var upperLetters = function(letters) {
-//   return letters.toUpperCase();
-// }
-
-//   console.log(upperLetters);
-
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 // Calculate number of each requested parameter to append to password
@@ -82,10 +67,34 @@ var tempPass = []
     tempPass.push(specialCharacters);
   } 
   
-    console.log(tempPass)
+    console.log(`Temp pass is ${tempPass}!`)
 // Edit tempPass variable to generate final password based on user requests
-//
 
+
+// Populate Password to replacement placeholder on page
+
+// passwordText.textContent = "test";
+
+// create random number to put into for loops
+for (let i = 0; i < countLength; i++) {
+  var random = Math.floor(Math.random() * 26);
+    console.log(letters[random]);
+}
+
+for (let i = 0; i < countLength; i++) {
+  var random = Math.floor(Math.random() * 26);
+    console.log(upperLetters[random]);
+}
+
+for (let i = 0; i < countLength; i++) {
+  var random = Math.floor(Math.random() * 10);
+    console.log(numbers[random]);
+}
+
+for (let i = 0; i < countLength; i++) {
+  var random = Math.floor(Math.random() * 10);
+    console.log(specialCharacters[random]);
+}
 
 
 
